1eb65c03cd7e0b93e5d60367f2644714b9ed7a11
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-11-05 13:55:31 +0100

    started implementing login and registration

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {
+    this.loginForm = new FormGroup({
+      email: new FormControl<string | null>('', Validators.required),
+      password: new FormControl<string | null>('', Validators.required)
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.email.value, this.password.value);
+    }
   }
 
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {
+    this.registerForm = new FormGroup({
+      email: new FormControl<string | null>('', [Validators.required, Validators.email]),
+      username: new FormControl<string | null>('', [Validators.required, Validators.minLength(3)]),
+      password: new FormControl<string | null>('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[\S]{6,}$/)]),
+      password2: new FormControl<string | null>('', Validators.required)
+    });
+   }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get password2() {
+    return this.registerForm.get('password2');
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.email.value, this.username.value, this.password.value, this.password2.value);
+    }
   }
 
 }
