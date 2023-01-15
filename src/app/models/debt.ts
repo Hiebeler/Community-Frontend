ab013037b4c73c6468771c39e09603fb3dbe3933
Hiebeler <emanuel.hiebeler@gmail.com>
2023-01-15 13:12:11 +0100

    debt show debt history

--- src/app/models/debt.ts
+++ src/app/models/debt.ts
@@ -5,14 +5,16 @@ export class Debt {
   public id: number;
   public name: string;
   public amount: number;
+  public timestamp: Date;
   public debitor: User;
   public creditor: User;
 
-  constructor(id: number, name: string, amount: number, debitor: User, creditor: User) {
+  constructor(id: number, name: string, amount: number, timestamp: Date, debitor: User, creditor: User) {
     this.id = id;
     this.name = name;
     this.amount = amount;
     this.debitor = debitor;
     this.creditor = creditor;
+    this.timestamp = timestamp;
   }
 }
