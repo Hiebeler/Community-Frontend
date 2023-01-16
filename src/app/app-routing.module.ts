2cc5321b8208620ae5a19ea92d7492fee9715d2f
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-01-16 16:51:43 +0100

    secured routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,10 +26,6 @@ const routes: Routes = [
   {
     path: 'task',
     loadChildren: () => import('./modals/task/task.module').then( m => m.TaskPageModule)
-  },
-  {
-    path: 'routines',
-    loadChildren: () => import('./pages/routines/routines.module').then( m => m.RoutinesPageModule)
   }
 ];
 @NgModule({
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 
 import { TabsPage } from './tabs.page';
 
@@ -10,31 +11,38 @@ const routes: Routes = [
     children: [
       {
         path: 'find-community',
-        loadChildren: () => import('../find-community/find-community.module').then( m => m.FindCommunityPageModule)
+        loadChildren: () => import('../find-community/find-community.module').then( m => m.FindCommunityPageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'create-community',
-        loadChildren: () => import('../create-community/create-community.module').then( m => m.CreateCommunityPageModule)
+        loadChildren: () => import('../create-community/create-community.module').then( m => m.CreateCommunityPageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'profile',
-        loadChildren: () => import('../profile/profile.module').then( m => m.ProfilePageModule)
+        loadChildren: () => import('../profile/profile.module').then( m => m.ProfilePageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'tasks',
-        loadChildren: () => import('../tasks/tasks.module').then( m => m.TasksPageModule)
+        loadChildren: () => import('../tasks/tasks.module').then( m => m.TasksPageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'tasks/routines',
-        loadChildren: () => import('../routines/routines.module').then( m => m.RoutinesPageModule)
+        loadChildren: () => import('../routines/routines.module').then( m => m.RoutinesPageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'shopping-list',
-        loadChildren: () => import('../shopping-list/shopping-list.module').then( m => m.ShoppingListPageModule)
+        loadChildren: () => import('../shopping-list/shopping-list.module').then( m => m.ShoppingListPageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'debts',
-        loadChildren: () => import('../debts/debts.module').then( m => m.DebtsPageModule)
+        loadChildren: () => import('../debts/debts.module').then( m => m.DebtsPageModule),
+        canActivate: [AuthGuard]
       },
       {
         path: '',
