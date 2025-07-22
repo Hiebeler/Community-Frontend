f61481cf8fb983356ba35b2cc88c5bdff554f0db
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-07-22 08:08:00 +0200

    Start implementing tailwind

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.page.html',
     styleUrls: ['./register.page.scss'],
-    standalone: false
+    standalone: true,
+    imports: [
+      IonicModule,
+      CommonModule,
+      ReactiveFormsModule
+    ]
 })
 export class RegisterPage {
 
