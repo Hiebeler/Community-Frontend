2f284124ce08a1ccc9e5c0628963c620538fe728
Hiebeler <emanuel.hiebeler@gmail.com>
2025-07-24 12:52:07 +0200

    change firstname to name

--- src/app/adapter/user-adapter.ts
+++ src/app/adapter/user-adapter.ts
@@ -15,7 +15,7 @@ export class UserAdapter implements Adapter<User> {
     item.creationdate = new Date(item.creationdate);
     item.profile_image = this.domSanitizer.bypassSecurityTrustResourceUrl(item.profile_image);
     return new User(
-      item.id, item.email, item.firstname, item.profile_image,
+      item.id, item.email, item.name, item.profile_image,
       item.creationdate, item.fk_community_id, item.color, item.communities);
   }
 }
