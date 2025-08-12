95237e421b17ff2fcea51fc68637a101e4b72c56
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-08-12 16:36:02 +0200

    Fix verify page

--- src/app/pages/verify/verify.page.ts
+++ src/app/pages/verify/verify.page.ts
@@ -1,23 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Loader2Icon, LucideAngularModule } from 'lucide-angular';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-    selector: 'app-verify',
-    templateUrl: './verify.page.html',
-    imports: [
-      CommonModule,
-    ]
+  selector: 'app-verify',
+  templateUrl: './verify.page.html',
+  imports: [CommonModule, RouterModule, LucideAngularModule],
 })
 export class VerifyPage implements OnInit {
+  readonly loaderIcon = Loader2Icon;
+
   code: string | null = '';
-  constructor(private route: ActivatedRoute, private authService: AuthService
-  ) { }
+
+  isLoading = false;
+  error = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.code = this.route.snapshot.paramMap.get('code');
-    this.authService.verify(this.code);
-  }
 
+    this.isLoading = true;
+    this.authService.verify(this.code).subscribe((res) => {
+      this.isLoading = false;
+      this.error = res.success ? '' : res.error;
+    });
+  }
 }
