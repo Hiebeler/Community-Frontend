c45f3ea677ba0a94ae06fce092c5e718ca2d9288
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-08-27 12:40:08 +0200

    finish footer translation

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, TranslocoModule],
   templateUrl: './footer.html',
 })
 export class Footer {}
