6fcfacf5b82e87406b79dba2866f3f52c120e4bd
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-08-27 13:49:37 +0200

    finish language switcher

--- src/app/components/language-switcher/language-switcher.ts
+++ src/app/components/language-switcher/language-switcher.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-language-switcher',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './language-switcher.html',
 })
 export class LanguageSwitcher {
+  activeLang: string;
 
+  constructor(private translocoService: TranslocoService) {
+    this.activeLang = translocoService.getActiveLang();
+  }
+
+  toggleLang() {
+    if (this.activeLang == "en") {
+      this.translocoService.setActiveLang("de");
+      this.activeLang = "de";
+    } else {
+      this.translocoService.setActiveLang("en");
+      this.activeLang = "en";
+    }
+  }
 }
